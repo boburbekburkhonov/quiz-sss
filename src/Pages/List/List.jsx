e7a9913b97339ee15330ecbd9271f5e6df6b057a
boburbekburkhonov <zafaroviccc@gmail.com>
2023-12-17 19:59:03 +0500

    feat: .

--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './List.css'
 import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
@@ -16,10 +16,15 @@ import FormLabel from '@mui/material/FormLabel';
 import smart from '../../assets/logo.svg'
 import user from '../../assets/user-logout.png'
 import logOut from '../../assets/logout.png'
+import { api } from '../API/api';
+import { Alert } from '@mui/material';
 
 const List = () => {
   const[quizResult, setQuizResult] = useState([])
+  const[userId, setUserId] = useState()
   const[allResult, setAllResult] = useState([])
+  const[listUser, setListUser] = useState([])
+  const[answersUsers, setAnswersUsers] = useState([])
   const [steps, setSteps] = useState([
     {
       label: '1-Savol',
@@ -65,6 +70,7 @@ const List = () => {
   ])
   const token = localStorage.getItem('token')
   const name = localStorage.getItem('name')
+  const repliedUser = localStorage.getItem('userId')
 
   if(!token){
     location.href = '/'
@@ -74,6 +80,26 @@ const List = () => {
     localStorage.clear()
     location.href = '/'
   }
+
+  useEffect(() => {
+    // ! LIST USERS
+    fetch(`${api}/list/users`, {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+    .then(res => res.json())
+    .then(data => setListUser(data))
+
+    // ! ANSWERS USERS
+    fetch(`${api}/answers/users`, {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+    .then(res => res.json())
+    .then(data => setAnswersUsers(data.data))
+  }, [])
 
   const [activeStep, setActiveStep] = React.useState(0);
 
@@ -97,16 +123,113 @@ const List = () => {
       [whichQuestion]: result != undefined ? result : 0
     })
     if(whichQuestion == 10){
-      console.log('finish');
-      window.location.reload()
+      const one = Object.values(allResult[0])[0]
+      const two = Object.values(allResult[1])[0]
+      const three = Object.values(allResult[2])[0]
+      const four = Object.values(allResult[3])[0]
+      const five = Object.values(allResult[4])[0]
+      const six = Object.values(allResult[5])[0]
+      const seven = Object.values(allResult[6])[0]
+      const eight = Object.values(allResult[7])[0]
+      const nine = Object.values(allResult[8])[0]
+      const ten = Object.values(allResult[9])[0]
+
+      fetch(`${api}/answers/users`, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+           user: userId,
+           answers: [{
+            answer_one: one,
+            answer_two: two,
+            answer_three: three,
+            answer_four: four,
+            answer_five: five,
+            answer_six: six,
+            answer_seven: seven,
+            answer_eight: eight,
+            answer_nine: nine,
+            answer_ten: ten,
+            replied_user: repliedUser
+          }]
+        }),
+    })
+    .then(res => res.json())
+    .then(data => {
+      if(data.status == 200){
+        window.location.reload()
+      }
+    })
     }
-    console.log(allResult);
     setQuizResult([])
   }
 
-  const postResult = e => {
-    e.preventDefault()
-    const {rowRadio}  = e.target
+  const foundUserName = id => {
+    const foundUser = listUser.find(e => e._id == id)
+
+    return `${foundUser.first_name} ${foundUser.middle_name} ${foundUser.last_name}`
+  }
+
+  const valueTable = (array, question) => {
+    if(question == 1){
+      const sumArr = array.reduce((a, b) => a + b.answer_one * 1, 0)
+      return sumArr
+    } else if(question == 2){
+      const sumArr = array.reduce((a, b) => a + b.answer_two * 1, 0)
+      return sumArr
+    } else if(question == 3){
+      const sumArr = array.reduce((a, b) => a + b.answer_three * 1, 0)
+      return sumArr
+    } else if(question == 4){
+      const sumArr = array.reduce((a, b) => a + b.answer_four * 1, 0)
+      return sumArr
+    } else if(question == 5){
+      const sumArr = array.reduce((a, b) => a + b.answer_five * 1, 0)
+      return sumArr
+    } else if(question == 6){
+      const sumArr = array.reduce((a, b) => a + b.answer_six * 1, 0)
+      return sumArr
+    } else if(question == 7){
+      const sumArr = array.reduce((a, b) => a + b.answer_seven * 1, 0)
+      return sumArr
+    } else if(question == 8){
+      const sumArr = array.reduce((a, b) => a + b.answer_eight * 1, 0)
+      return sumArr
+    } else if(question == 9){
+      const sumArr = array.reduce((a, b) => a + b.answer_nine * 1, 0)
+      return sumArr
+    } else if(question == 10){
+      const sumArr = array.reduce((a, b) => a + b.answer_ten * 1, 0)
+      return sumArr
+    }
+  }
+
+  const valueAverage = array => {
+    const answer_one = array.reduce((a, b) => a + b.answer_one * 1, 0)
+    const answer_two = array.reduce((a, b) => a + b.answer_two * 1, 0)
+    const answer_three = array.reduce((a, b) => a + b.answer_three * 1, 0)
+    const answer_four = array.reduce((a, b) => a + b.answer_four * 1, 0)
+    const answer_five = array.reduce((a, b) => a + b.answer_five * 1, 0)
+    const answer_six = array.reduce((a, b) => a + b.answer_six * 1, 0)
+    const answer_seven = array.reduce((a, b) => a + b.answer_seven * 1, 0)
+    const answer_eight = array.reduce((a, b) => a + b.answer_eight * 1, 0)
+    const answer_nine = array.reduce((a, b) => a + b.answer_nine * 1, 0)
+    const answer_ten = array.reduce((a, b) => a + b.answer_ten * 1, 0)
+
+    return ((answer_one + answer_two + answer_three + answer_four + answer_five + answer_six + answer_seven + answer_eight + answer_nine + answer_ten) / array.length).toFixed(1)
+  }
+
+  const answeredOrNot = id => {
+    const foundUser = answersUsers.find(e => e.user == id)
+    console.log(foundUser);
+    const result = foundUser?.answers.find(e => e.replied_user == repliedUser)
+    if(!result){
+      return false
+    }else {
+      return true
+    }
   }
 
   return (
@@ -120,7 +243,18 @@ const List = () => {
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div className="modal-body m-auto">
-                <Box sx={{ maxWidth: 400 }}>
+                {
+                  answeredOrNot(userId)
+                  ?
+                  <div>
+                    <img src={smart} alt="smart" />
+                    <h3 className='h-100 d-flex align-items-center'>
+                      Bu xodimga ovoz berib bo'lgansiz!
+                    </h3>
+                  </div>
+
+                  :
+                  <Box sx={{ maxWidth: 400 }}>
                   <Stepper activeStep={activeStep} orientation="vertical">
                     {steps?.map((step, index) => (
                       <Step key={step.label}>
@@ -163,6 +297,7 @@ const List = () => {
                     ))}
                   </Stepper>
                 </Box>
+                }
               </div>
             </div>
           </div>
@@ -180,10 +315,10 @@ const List = () => {
             <div className='d-flex align-items-center dropdown-toggle' data-bs-toggle="dropdown" aria-expanded="false">
               <img src={user} alt="user" width={30} height={30} />
               <p className='m-0 fs-5 ms-2'>{name}</p>
-              <ul class="dropdown-menu m-0 p-0">
+              <ul className="dropdown-menu m-0 p-0">
                 <li className='d-flex align-items-center' onClick={logout}>
                   <img className='ms-3' src={logOut} alt="logOut" width={20} height={20} />
-                  <button class="dropdown-item p-0 py-2 ms-2 fw-semibold">Chiqish</button>
+                  <button className="dropdown-item p-0 py-2 ms-2 fw-semibold">Chiqish</button>
                 </li>
               </ul>
             </div>
@@ -218,149 +353,46 @@ const List = () => {
                     <th colSpan='5'>Ishga aloqasi bo’lmaganlar bilan telefonda ko’p gaplashishi</th>
                     <th>Tozalikka rioya qilishi</th>
                     <th>Berilgan topshiriqlarni bajarishi</th>
+                    <th>O'rtacha</th>
                   </tr>
                 </thead>
                 <tbody>
-                <tr
-                  className="tr0"
-                  data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                >
-                  <td className="sticky" style={{}}>
-                    1
-                  </td>
-                  <td
-                    className="text-start sticky fix-with"
-                    style={{ left: "57px" }}
+                {
+                  answersUsers.map((e, i) => {
+                    return <tr
+                    className="tr0"
+                    key={i}
+                    data-bs-toggle="modal" data-bs-target="#staticBackdrop"
+                    onClick={() => setUserId(e.user)}
                   >
-                    <div className="d-flex align-items-center justify-content-center">
-                      <span className="fs-6 fw-normal">
-                        Ehsmaty
-                      </span>
-                    </div>
-                  </td>
-                  <td colSpan='2'>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td colSpan='3'>-</td>
-                  <td colSpan='5'>-</td>
-                  <td colSpan='5'>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-                <tr
-                  className="tr0"
-                  data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                >
-                  <td className="sticky" style={{}}>
-                    1
-                  </td>
-                  <td
-                    className="text-start sticky fix-with"
-                    style={{ left: "57px" }}
-                  >
-                    <div className="d-flex align-items-center justify-content-center">
-                      <span className="fs-6 fw-normal">
-                        Ehsmaty
-                      </span>
-                    </div>
-                  </td>
-                  <td colSpan='2'>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td colSpan='3'>-</td>
-                  <td colSpan='5'>-</td>
-                  <td colSpan='5'>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-                <tr
-                  className="tr0"
-                  data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                >
-                  <td className="sticky" style={{}}>
-                    1
-                  </td>
-                  <td
-                    className="text-start sticky fix-with"
-                    style={{ left: "57px" }}
-                  >
-                    <div className="d-flex align-items-center justify-content-center">
-                      <span className="fs-6 fw-normal">
-                        Ehsmaty
-                      </span>
-                    </div>
-                  </td>
-                  <td colSpan='2'>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td colSpan='3'>-</td>
-                  <td colSpan='5'>-</td>
-                  <td colSpan='5'>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-                <tr
-                  className="tr0"
-                  data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                >
-                  <td className="sticky" style={{}}>
-                    1
-                  </td>
-                  <td
-                    className="text-start sticky fix-with"
-                    style={{ left: "57px" }}
-                  >
-                    <div className="d-flex align-items-center justify-content-center">
-                      <span className="fs-6 fw-normal">
-                        Ehsmaty
-                      </span>
-                    </div>
-                  </td>
-                  <td colSpan='2'>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td colSpan='3'>-</td>
-                  <td colSpan='5'>-</td>
-                  <td colSpan='5'>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-                <tr
-                  className="tr0"
-                  data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                >
-                  <td className="sticky" style={{}}>
-                    1
-                  </td>
-                  <td
-                    className="text-start sticky fix-with"
-                    style={{ left: "57px" }}
-                  >
-                    <div className="d-flex align-items-center justify-content-center">
-                      <span className="fs-6 fw-normal">
-                        Lorem, ipsum dolor.
-                      </span>
-                    </div>
-                  </td>
-                  <td colSpan='2'>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td colSpan='3'>-</td>
-                  <td colSpan='5'>-</td>
-                  <td colSpan='5'>-</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
+                    <td className="sticky" style={{}}>
+                      {i + 1}
+                    </td>
+                    <td
+                      className="text-start sticky fix-with"
+                      style={{ left: "57px" }}
+                    >
+                      <div className="d-flex align-items-center justify-content-center">
+                        <span className="fs-6 fw-normal">
+                          {foundUserName(e.user)}
+                        </span>
+                      </div>
+                    </td>
+
+                    <td colSpan='2'>{valueTable(e.answers, 1)}</td>
+                    <td>{valueTable(e.answers, 2)}</td>
+                    <td>{valueTable(e.answers, 3)}</td>
+                    <td>{valueTable(e.answers, 4)}</td>
+                    <td>{valueTable(e.answers, 5)}</td>
+                    <td colSpan='3'>{valueTable(e.answers, 6)}</td>
+                    <td colSpan='5'>{valueTable(e.answers, 7)}</td>
+                    <td colSpan='5'>{valueTable(e.answers, 8)}</td>
+                    <td>{valueTable(e.answers, 9)}</td>
+                    <td>{valueTable(e.answers, 10)}</td>
+                    <td>{valueAverage(e.answers)}</td>
+                  </tr>
+                  })
+                }
                 </tbody>
               </table>
             </div>
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import './Login.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { api } from '../API/api.js';
+import smart from '../../assets/logo.svg'
+
 
 const Login = () => {
     const [error, setError] = useState('')
@@ -58,7 +60,6 @@ const Login = () => {
         })
         .then(res => res.json())
         .then(data => {
-            console.log(data);
             if(data.status == 200){
                 localStorage.setItem('userId', data.message.data._id)
                 localStorage.setItem('userName', data.message.data.user_username)
@@ -81,7 +82,10 @@ const Login = () => {
         <div className="form-container sign-up-container">
 
             <form onSubmit={sigin} id="contact-form" className="main-form needs-validation">
-                <h3>Akkount yaratish</h3>
+                <div className='d-flex align-items-center w-100'>
+                    <img src={smart} alt="smart" width={75} height={100} />
+                    <h3 className='fs-5 m-0'>Akkount yaratish</h3>
+                </div>
                 <div className="form-group mt-3">
                   <label>
                       <input name='username' type="text" id="form_user" className="my_form-control" required />
@@ -111,7 +115,10 @@ const Login = () => {
 
         <div className="form-container sign-in-container">
             <form onSubmit={login} id="contact-form" className="main-form needs-validation">
-                <h3>Kirish</h3>
+                <div className='d-flex align-items-center'>
+                    <img src={smart} alt="smart" width={95} height={100} />
+                    <h3 className='fs-5 m-0'>Kirish</h3>
+                </div>
 
                 <div className="form-group">
                     <label>
